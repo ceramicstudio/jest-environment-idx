eaf63f52c5739faa3609cf2b44e39fb62f421c55
Paul Le Cam <paul@3box.io>
2020-12-01 17:09:11 +0100

    Use key-did-provider-ed25519 and new version of IDX tools

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const { publishIDXConfig } = require('@ceramicstudio/idx-tools')
-const Wallet = require('identity-wallet').default
 const CeramicEnvironment = require('jest-environment-ceramic')
+const { Ed25519Provider } = require('key-did-provider-ed25519')
 const fromString = require('uint8arrays/from-string')
 
 module.exports = class IDXEnvironment extends CeramicEnvironment {
   constructor(config, context) {
     super(config, context)
-    this.seed = fromString(
-      config.seed || '0000000000000000000000000000000000000000000000000000000000000000'
-    )
+    this.seed = config.seed ? fromString(config.seed) : new Uint8Array(32)
   }
 
   async setup() {
     await super.setup()
-    await publishIDXConfig(this.global.ceramic)
-    this.global.wallet = await Wallet.create({
-      ceramic: this.global.ceramic,
-      seed: this.seed,
-      getPermission: () => Promise.resolve([]),
-    })
-    await this.global.ceramic.setDIDProvider(this.global.wallet.getDidProvider())
+    await Promise.all([
+      publishIDXConfig(this.global.ceramic),
+      this.global.ceramic.setDIDProvider(new Ed25519Provider(this.seed)),
+    ])
   }
 }
